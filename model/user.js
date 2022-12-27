007bc9f1fc3e8f76b29ff72761427051e9afd934
JeeIn-Shin <wldls0530@gmail.com>
2022-12-27 23:06:23 +0900

    Implementing profile related api, not tested

--- model/user.js
+++ model/user.js
@@ -1,49 +1,146 @@
+const { getConnection } = require("../config/database.js");
 const db = require("../config/database.js");
 
 const User = function (input_user_data) {
     this.student_id = input_user_data.student_id;
     this.password = input_user_data.password;
-    this.name = input_user_data.name;
-    this.email = input_user_data.email;
-    this.nickname = input_user_data.nickname;
+    this.git_hub = input_user_data.git_hub;
+    this.profile_img = input_user_data.profile_img;
 };
 
 //사용자 회원가입
 //student id, email, nickname UQ
 //input_user_data => 사용자로부터 입력받은 객체
-User.create = (input_user_data, result) => {
+
+//회원가입하고 아무것도 안하면 그냥 기본 이미지인거지
+//오 이게 좋은거 같음
+User.create = (init, result) => {
     
-    let query_create_user = `INSERT INTO user
-                            values
-                            ('${input_user_data.student_id}', '${input_user_data.password}', '${input_user_data.name}',
-                            '${input_user_data.email}', '${input_user_data.nickname}', '${input_user_data.position}')`;
-    
-    db.init().query(query_create_user, (err, res) => {
-        if (err) {
-            console.log("error: ", err);
-            result(err, null);
-            return;
+    let setting_value = Object.values(init);
+
+    db.getConnection((err, connection) => {
+        if(!err) {
+            let SQL = `INSERT INTO user VALUES ( ?, '../view/img/profile/default.png' )`;
+            connection.query(SQL, [setting_value], (err, res) => {
+                connection.release();
+
+                if(err) {
+                    console.log("error " + err);
+                    result(null, err);
+                    return ;
+                }
+
+                result(null, res);
+                return ;
+            })
         }
-        console.log("created user: ", { id: res.student_id, ...newUser });
-        result(null, { id: res.student_id, ...newUser });
-    });
+        else    {
+            console.error();
+            throw err;
+        }
+    })
 };
 
-User.findById = (student_id, result) =>{
-    
-    let query_search_by_id = `SELECT * FROM user WHERE student_id like ${student_id}`;
-    
-    db.init().query((query_search_by_id), (err, res) => {
-        if(err){
-            console.log("error: ", err);
-            result(err, null);
-            return;
-        }
 
-        console.log("user: ", res);
-        result(null, res);
-    });
+//해당 사용자의 정보 불러오기
+User.findById = (adminID, result) =>{
+
+
+    db.getConnection((err, connection) => {
+        if(!err) {
+            
+            let SQL = `SELECT git_hub, nickname, profile_img FROM user 
+                       WHERE student_id LIKE ${adminID}`;
+            
+            connection.query(SQL, (err, res) => {
+                connection.release();
+
+                if(err) {
+                    console.log("error " + err);
+                    result(null, err);
+                    return ;
+                }
+                result(null, res);
+                return ;
+            })
+        }
+        else    {
+            console.error();
+            throw err;
+        }
+    })
+
 };
 
+//사용자의 정보 변경
+//프로필 사진 제외
+
+//ㅈㅁ 이거 고민 좀 해보자..
+//비밀번호 변경을 따로 둬야하지 않을까?
+User.update = (adminID, input_values, result) => {
+
+    let setting_value = Object.values(input_values);
+
+    db.getConnection(function(err, connection) {
+        
+        if(!err) {
+            let SQL = `UPDATE user SET
+                       email = ?, nickname = ?, git_hub = ?
+                       WHERE user_student_id like ${adminID}`;
+
+            connection.query(SQL, setting_value, (err, res) => {
+                connection.release();
+
+                if(err) {
+                    console.log("error " + err);
+                    result(null, err);
+                    return ;
+                }
+                result(null, res);
+                return ;
+            })
+        }
+        else    {
+            console.error();
+            throw err;
+        }
+    })
+}
+
+
+//사용자의 프로필 사진 변경
+User.update_profile = (adminID, result) => {
+    db.getConnection(function(err, connection) {
+        if(!err) {
+
+            let SQL = ``;
+
+            connection.query(SQL, (err, res) => {
+                connection.release();
+
+                if(err) {
+                    console.log("error " + err);
+                    result(null, err);
+                    return ;
+                }
+                
+                result(null, res);
+                return ;
+            })
+        }
+        else    {
+            console.error();
+            throw err;
+        }
+    })
+}
+
+//사용자 비밀번호 변경
+//근데 암호화 되어있는데 이걸 어떻게?
+//메일링 서비스 연동
+//나중에 구현
+User.update_password = () => {
+
+}
 
 module.exports = User;
--- router/user.js
+++ router/user.js
@@ -1,63 +1,52 @@
 const User = require('../model/user');
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 
-//http://localhost:8080/user/create
-router.post('/create', async(req, res) => {
-    //Validate request
-    if (!req.body) {
-        res.status(400).send({
-            message: "Content can not be empty!"
-        });
-    }
+//프로필 화면에서 사용자의 가입 시 기본 프로필을 보여줌
+// http://localhost:8080/user/profile/:studentID
+router.get('/profile/:studentID', async(req, res) => {
 
-    //Create a User
-    //입력 받는건  id, password, name, email, nickname
-    //position 은 기본값 0으로 설정
-    //position은 추후 수정될 가능성 농후
-    //positon X , column이 reviewee, reviewer로 쪼개진다던가..
-    const user = new User({
-        student_id: req.body.student_id,
-        password: req.body.password,
-        name: req.body.name,
-        email: req.body.email,
-        nickname: req.body.nickname,
-        positon: 0    // default(0), reviewee(1), reviewer(2)
-    });
+    //로그인 된게 맞는지
 
-    //let input_user_data = await User.create(user)
+    //아니라면 ..
 
-    // Save User in the database
-    User.create(input_user_data, (err, data) => {
-        
-        //문제 발생, 값이 입력받아지지않음
-        //어째서..!
-        console.log(input_user_data.student_id);
-        if (err)
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while creating the User."
-            });
-        else res.json(data);
-    });
+    await User.findById(req.params, (err, data) => {
+        try {
+            res.json(data);
+        }
+        catch(err)  {
+            console.error(err);
+        }
+    })
 })
 
 
-// http
-router.get('/:student_id', (req, res) => {
-    User.findById(req.params.student_id, (err, data) => {
-        if (err) {
-            if (err.kind === "not_found") {
-                res.status(404).send({
-                    message: `Not found student with id ${req.params.student_id}.`
-                });
-            } else {
-                res.status(500).send({
-                    message: "Error retrieving student with id " + req.params.student_id
-                });
-            }
-        } else res.json(data);
-    });
-});
+// http://localhost:8080/user/profile/update/:studentID
+router.put('/profile/update/:studentID', async(req, res) => {
+    
+    //로그인 된게 맞는지
+
+    //이메일, 닉네임, 깃허브 중복없는지
+
+
+    let update_value = {
+        email: req.body.email,
+        nickname: req.body.email,
+        git_hub: req.body.git_hub
+    }
+    
+    
+    await User.update(req.params, update_value, (err, data) => {
+        try {
+            res.json(data);
+        }
+        catch(err) {
+            console.error(err);
+        }
+    })
+})
 
 module.exports = router;
