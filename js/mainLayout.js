41cdf6c8ca5c7ac1b81b15a252ef41f91f0691b4
jin4344 <wlstj4344@gmail.com>
2023-01-10 14:57:46 +0900

    Main Layout 1 / 01.10

---
+++ js/mainLayout.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $("#bodys").load("test.html");
+});
+
+// 네비게이션 바의 목록 클릭 시 바디에 불러올 html 바꾸기
+// 아직 구상 중
+// $(() => {
+//     $("#bodys > #test").click(() => {
+//         $("#body").load("/");
+//     });
+// });
