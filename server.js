c27b134b767b716330c278495da0533fde8164fa
JeeIn-Shin <wldls0530@gmail.com>
2023-01-27 04:05:05 +0900

    install path module

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const path = require('path');
 
 const PROFILEROUTER = require('./routers/profile');
 const SIGNUPROUTER = require("./routers/signup");
+const MATCHINGROUTER = require("./routers/matching");
 
 const app = express();
 
@@ -14,14 +16,17 @@ app.set('port', process.env.PORT || 8080);
 //view engine 설정 이렇게 하는게 맞나..?
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
+
 // app.get('/', (req, res) =>{
 //     res.json({ test : "test"});
 // });
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/profile', PROFILEROUTER);
 app.use('/signup', SIGNUPROUTER);
+app.use('/walk-thru', MATCHINGROUTER);
 
 
 app.listen(app.get('port'), () => {
