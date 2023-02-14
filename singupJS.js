86dc2c76830badfda4972aaf832eb2ca33fda696
sOmin22 <122062975+sOmin22@users.noreply.github.com>
2023-02-14 16:33:28 +0900

    폼 유효성 검사
    각 유효성 검사 진행 1

--- singupJS.js
+++ singupJS.js
@@ -17,8 +17,31 @@ fileInput.addEventListener("change", handleFiles);
 //
 
 
-//비밀번호 조건 함수
-function check_pw() {
-    let pw = document.getElementById('pw').value;
-    let checkpw = /^[A-Za-z0-9`~!@#\$%\^&\*\(\)\{\}\[\]\-_=\+\\|;:'"<>,\./\?]{8,20}$/;
+//이메일 검사
+const emailCheck = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+
+
+
+
+//패스워드 유효성 검사
+const pwCheck = /^.*(?=^.{8,16}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[~,!,@,#,$,*,(,),=,+,_,.,|]).*$/;
+
+
+
+
+//패스워드 체크 검증
+function passCheck2 () {
+  let pw = document.getElementById("joinps").value;
+  let pwch = document.getElementById("checkps").value;
+
+  if (pw != pwch) {
+    document.getElementById("pwCheckError").innerHTML("비밀번호가 일치하지 않습니다. 다시 입력해 주세요.");
+    return false;
+  }
 }
+
+ 
+
+
+//이름 검증
+const nameCheck = /^[a-z0-9_-]{2,20}$/;
