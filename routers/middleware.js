628fae4ef137c190d84710acd7160917acf45e02
JeeIn-Shin <wldls0530@gmail.com>
2023-03-07 00:14:28 +0900

    temporary

--- routers/middleware.js
+++ routers/middleware.js
@@ -12,3 +12,18 @@ exports.isNotLoggedIn = (req, res, next) => {
     else
         res.status(400).send('로그인이 되어있습니다');
 };
+
+exports.isAdminLoggedIn = (req, res, next) => {
+    if (req.isAuthenticated())
+        return next();
+    else
+        res.status(401).send('로그인을 해주세요');
+}
+
+exports.isNotAdminLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        return next();
+    }
+    else
+        res.status(400).send('로그인이 되어있습니다');
+}
--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -44,7 +47,7 @@ app.set('port', process.env.PORT || 8080);
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine','ejs');
+app.set('view engine','html');
 app.engine('html', require('ejs').__express);
 
 
