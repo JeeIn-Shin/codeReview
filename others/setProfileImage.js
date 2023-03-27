baa7cc4d4a6809075859c3fe4305f6b67e360812
JeeIn-Shin <wldls0530@gmail.com>
2023-03-28 03:24:26 +0900

    case 조건, 숫자에서 문자열로 변경

--- others/setProfileImage.js
+++ others/setProfileImage.js
@@ -1,24 +1,24 @@
 let setProfileImage = (imageUpdateValue) => {
     switch(imageUpdateValue)  {
-        case 0:
+        case "0":
             return '../view/img/profile/0.png';
             break;
-        case 1:
+        case "1":
             return '../view/img/profile/1.png';
             break;
-        case 2:
+        case "2":
             return '../view/img/profile/2.png';
             break;
-        case 3:
+        case "3":
             return '../view/img/profile/3.png';
             break;
-        case 4:
+        case "4":
             return '../view/img/profile/4.png';
             break;
-        case 5:
+        case "5":
             return '../view/img/profile/5.png';
             break;
-        case 6:
+        case "6":
             return '../view/img/profile/6.png';
             break;
     }
