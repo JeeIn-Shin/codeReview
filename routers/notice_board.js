ad3b537bfd0167aac67f843b99bef01d5d700add
JeeIn-Shin <wldls0530@gmail.com>
2023-04-03 03:18:00 +0900

    게시판 파일 이름 정리

--- routers/notice_board.js
+++ routers/notice_board.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const noticeboard = require('../models/noticeBoard');
+const noticeboard = require('../models/notice_board');
 
 
 router.get('/', async(req, res) => {
