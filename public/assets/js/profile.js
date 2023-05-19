7e182ea1b5488c8718e33115741c43d48075a383
sOmin22 <122062975+sOmin22@users.noreply.github.com>
2023-05-20 00:12:47 +0900

    개인정보 수정 페이지 및 수정
    개인정보 수정 페이지 폼 수정중/ 회원가입 비밀번호 유효성

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -16,3 +16,26 @@ function select(e)  //이미지 버튼 클릭시 색상 변경 저장 함수
     });
     console.log(e.currentTarget);
 }
+
+
+function validatePassword() {
+    var password = document.getElementById("password").value;
+    var passwordch = document.getElementById("passwordch").value;
+
+    if (password === passwordch) {
+        showMessage("비밀번호가 일치합니다.", false);
+    } else {
+        showMessage("비밀번호가 일치하지 않습니다.", true);
+    }
+}
+
+    function showMessage(message, isError) {
+        var messageElement = document.getElementById("message");
+        messageElement.innerHTML = message;
+
+        if (isError) {
+            messageElement.classList.add("error");
+        } else {
+            messageElement.classList.remove("error");
+        }
+    }
---
+++ public/assets/js/reprofile.js
@@ -0,0 +1,38 @@
+    function showForm() {
+        var passwordInput = document.getElementById("passwordInput");
+        var password = passwordInput.value;
+
+        // 현재 비밀번호가 일치할 경우 개인정보 수정 폼 표시
+        if (password === "11") {
+        var passwordContainer = document.getElementById("passwordContainer");
+        passwordContainer.style.display = "none";
+        var formContainer = document.getElementById("formContainer");
+        formContainer.style.display = "block";
+        } else {
+        alert("현재 비밀번호가 일치하지 않습니다.");
+        }
+    }
+
+    
+  
+  
+  
+  function validateForm() {
+    
+    var newPassword = document.getElementById("newPassword").value;
+    var confirmNewPassword = document.getElementById("confirmNewPassword").value;
+
+
+    if (newPassword !== confirmNewPassword) {
+      var errorMessage = document.getElementById("errorMessage");
+      errorMessage.textContent = "새 비밀번호가 일치하지 않습니다.";
+      return false;
+    }
+
+    // 추가적인 유효성 검사 로직을 작성할 수 있습니다.
+
+    alert("개인정보가 수정되었습니다.");
+    return true;
+  }
+
+
