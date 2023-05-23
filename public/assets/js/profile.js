d0391ba20e6a085be48ede5835b2a8e247dea96d
sOmin22 <122062975+sOmin22@users.noreply.github.com>
2023-05-24 00:18:51 +0900

    파일명 변경 및 우선순위 수정

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -1,4 +1,41 @@
+              // function showForm() {
+              //     var passwordInput = document.getElementById("passwordInput");
+              //     var password = passwordInput.value;
 
+              //     // 현재 비밀번호가 일치할 경우 개인정보 수정 폼 표시
+              //     if (password === "11") {
+              //     var passwordContainer = document.getElementById("passwordContainer");
+              //     passwordContainer.style.display = "none";
+              //     var formContainer = document.getElementById("formContainer");
+              //     formContainer.style.display = "block";
+              //     } else {
+              //     alert("현재 비밀번호가 일치하지 않습니다.");
+              //     }
+              // }
+
+              
+  
+  
+  
+  function validateForm() {
+    
+    var newPassword = document.getElementById("newPassword").value;
+    var confirmNewPassword = document.getElementById("confirmNewPassword").value;
+
+
+    if (newPassword !== confirmNewPassword) {
+      var errorMessage = document.getElementById("errorMessage");
+      errorMessage.textContent = "새 비밀번호가 일치하지 않습니다.";
+      return false;
+    }
+
+    // 추가적인 유효성 검사 로직을 작성할 수 있습니다.
+
+    alert("개인정보가 수정되었습니다.");
+    return true;
+  }
+
+  
 
 function select(e)  //이미지 버튼 클릭시 색상 변경 저장 함수
 {
@@ -7,7 +44,7 @@ function select(e)  //이미지 버튼 클릭시 색상 변경 저장 함수
         if (e.currentTarget == btn) 
         {
             btn.classList.add("active");
-            $('input[name=profileImgNumber]').attr('value',(i+1).toString());
+            $('input[name=profileImg]').attr('value',(i+1).toString());
         } 
         else 
         {
@@ -17,27 +54,60 @@ function select(e)  //이미지 버튼 클릭시 색상 변경 저장 함수
     console.log(e.currentTarget);
 }
 
-
-function validatePassword() {
-    var password = document.getElementById("password").value;
-    var passwordch = document.getElementById("passwordch").value;
-
-    if (password === passwordch) {
-        showMessage("비밀번호가 일치합니다.", false);
-    } else {
-        showMessage("비밀번호가 일치하지 않습니다.", true);
-    }
-}
-
-    function showMessage(message, isError) {
-        var messageElement = document.getElementById("message");
-        messageElement.innerHTML = message;
-
-        if (isError) {
-            messageElement.classList.add("error");
-        } else {
-            messageElement.classList.remove("error");
+const form = document.getElementById('profileform');
+function getCookieValue(cookieName) 
+{      
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].split('=');
+        if (cookie[0] === cookieName) {
+        return cookie[1];
         }
     }
+    return '';
+}
 
-    
+form.addEventListener('submit', function(event) {
+event.preventDefault(); // 기본 동작인 폼 제출 방지
+$.ajax({
+    url: form.action,
+    type: form.method,
+    headers: {
+    Authorization: 'Bearer ' + getCookieValue('accessToken')
+    },
+    data: {
+    nickName: form.nickName.value,
+    github: form.github.value,
+    profileImg: form.profileImg.value,
+    C : form.c.value,
+    CPlus : form.cPlus.value, 
+    CSharp : form.cSharp.value,
+    java : form.java.value,
+    kotlin : form.kotlin.value,
+    swift : form.swift.value,
+    python : form.python.value,
+    go : form.go.value,
+    javascript : form.javascript.value,
+    rust : form.rust.value,
+    ruby : form.ruby.value,
+    codeReview : form.codeReview.value,
+    refactoring : form.refactoring.value,
+    qa : form.qa.value
+    }
+});
+});
+
+function is_checked() {
+    if(document.getElementById("codeReview").checked) 
+    {
+        document.getElementById("codeReview_hidden").disabled = true;
+    }
+    if(document.getElementById("refactoring").checked) 
+    {
+        document.getElementById("refactoring_hidden").disabled = true;
+    }
+    if(document.getElementById("qa").checked) 
+    {
+        document.getElementById("qa_hidden").disabled = true;
+    }
+}
---
+++ public/assets/js/signup.js
@@ -0,0 +1,57 @@
+
+
+function select(e)  //이미지 버튼 클릭시 색상 변경 저장 함수
+{
+    var btns = document.querySelectorAll(".button");
+    btns.forEach(function (btn, i) {
+        if (e.currentTarget == btn) 
+        {
+            btn.classList.add("active");
+            $('input[name=profileImgNumber]').attr('value',(i).toString());
+        } 
+        else 
+        {
+            btn.classList.remove("active");
+        }
+    });
+    console.log(e.currentTarget);
+}
+
+
+function validatePassword() {
+    var password = document.getElementById("password").value;
+    var passwordch = document.getElementById("passwordch").value;
+
+    if (password === passwordch) {
+        showMessage("비밀번호가 일치합니다.", false);
+    } else {
+        showMessage("비밀번호가 일치하지 않습니다.", true);
+    }
+}
+
+    function showMessage(message, isError) {
+        var messageElement = document.getElementById("message");
+        messageElement.innerHTML = message;
+
+        if (isError) {
+            messageElement.classList.add("error");
+        } else {
+            messageElement.classList.remove("error");
+        }
+    }
+
+    
+    function is_checked() {
+        if(document.getElementById("codereview").checked) 
+        {
+            document.getElementById("codereview_hidden").disabled = true;
+        }
+        if(document.getElementById("refactoring").checked) 
+        {
+            document.getElementById("refactoring_hidden").disabled = true;
+        }
+        if(document.getElementById("qa").checked) 
+        {
+            document.getElementById("qa_hidden").disabled = true;
+        }
+    }
