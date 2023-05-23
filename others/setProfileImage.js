ce27571f7a00c7e7111f3108677641be8daa6090
JeeIn-Shin <wldls0530@gmail.com>
2023-05-23 23:59:30 +0900

    경로수정

--- others/setProfileImage.js
+++ others/setProfileImage.js
@@ -1,25 +1,16 @@
 let setProfileImage = (imageUpdateValue) => {
     switch(imageUpdateValue)  {
         case "0":
-            return '../view/img/profile/0.png';
+            return '../public/assets/img/profile1.png';
             break;
         case "1":
-            return '../view/img/profile/1.png';
+            return '../public/assets/img/profile2.png';
             break;
         case "2":
-            return '../view/img/profile/2.png';
+            return '../public/assets/img/profile3.png';
             break;
         case "3":
-            return '../view/img/profile/3.png';
-            break;
-        case "4":
-            return '../view/img/profile/4.png';
-            break;
-        case "5":
-            return '../view/img/profile/5.png';
-            break;
-        case "6":
-            return '../view/img/profile/6.png';
+            return '../public/assets/img/profile4.png';
             break;
     }
 }
