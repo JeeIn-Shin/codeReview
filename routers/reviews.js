5beb6572f62baa4e80dd01437fd47642eb3af1c5
JeeIn-Shin <wldls0530@gmail.com>
2023-05-24 04:43:36 +0900

    parameter 값 잘못넣어서 수정함

--- routers/reviews.js
+++ routers/reviews.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const guestBook = require('../models/guestbook');
+const client = require('../models/user');
 const { checkTokens } = require('../middleware/auth');
 const jwt = require('jsonwebtoken');
-
 
 // http://localhost:8080/reviews
 router.get('/', checkTokens, async(req, res) => {
     let AT =  req.headers.authorization.split('Bearer ')[1];
     let user = jwt.decode(AT, process.env.secret);
-    
+    let fk = await client.signIn.getUserPKById(user.id);
     /// ?????
-    let reviewerList = await guestBook.getCodereviewActInfo(user.id);
+    let reviewerList = await guestBook.getCodereviewActInfo(fk);
 
     res.json(reviewerList);
 })
