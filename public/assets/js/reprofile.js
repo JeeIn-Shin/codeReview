8940801896da00cb9141d9044881364a83cf59be
sOmin22 <122062975+sOmin22@users.noreply.github.com>
2023-05-23 19:45:59 +0900

    개인정보 토큰 수정

--- public/assets/js/reprofile.js
+++ public/assets/js/reprofile.js
@@ -44,7 +44,7 @@ function select(e)  //이미지 버튼 클릭시 색상 변경 저장 함수
         if (e.currentTarget == btn) 
         {
             btn.classList.add("active");
-            $('input[name=profileImgNumber]').attr('value',(i+1).toString());
+            $('input[name=profileImg]').attr('value',(i+1).toString());
         } 
         else 
         {
@@ -54,4 +54,45 @@ function select(e)  //이미지 버튼 클릭시 색상 변경 저장 함수
     console.log(e.currentTarget);
 }
 
+const form = document.getElementById('profileform');
+function getCookieValue(cookieName) 
+{      
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].split('=');
+        if (cookie[0] === cookieName) {
+        return cookie[1];
+        }
+    }
+    return '';
+}
 
+form.addEventListener('submit', function(event) {
+event.preventDefault(); // 기본 동작인 폼 제출 방지
+$.ajax({
+    url: form.action,
+    type: form.method,
+    headers: {
+    Authorization: 'Bearer ' + getCookieValue('accessToken')
+    },
+    data: {
+    nickName: form.nickName.value,
+    github: form.github.value,
+    profileImg: form.profileImg.value,
+    C : form.c.value,
+    CPlus : form.cPlus.value, 
+    CSharp : form.cSharp.value,
+    java : form.java.value,
+    kotlin : form.kotlin.value,
+    swift : form.swift.value,
+    python : form.python.value,
+    go : form.go.value,
+    javascript : form.javascript.value,
+    rust : form.rust.value,
+    ruby : form.ruby.value,
+    codeReview : form.codeReview.value,
+    refactoring : form.refactoring.value,
+    qa : form.qa.value
+    }
+});
+});
