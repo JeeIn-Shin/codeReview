e27f87f305ebdd8523bc15371d29edd5bad2a8e6
JeeIn-Shin <wldls0530@gmail.com>
2023-05-25 02:35:42 +0900

    prefer activity 삭제

--- models/matching.js
+++ models/matching.js
@@ -44,14 +44,12 @@ const matching = {
         return new Promise((resolve, reject) => {
             let enroll = Object.values(enrollInfo);
             let plan = Object.values(additionalData);
-            let prefer = Object.values(preferData);
 
             db.getConnection((err, connection) => {
                 connection.beginTransaction((err) => {
                     if (!err) {
                         let sql1 = `INSERT INTO QUEUE_TB VALUES ( ? )`;
                         let sql2 = `INSERT INTO SCHEDULE_TB VALUES ( ? )`;
-                        let sql3 = `INSERT INTO REVIEWEE_PREFER_TB VALUES ( ? )`;
 
                         connection.query(sql1, [enroll], (err, res) => {
                             if (err)
@@ -60,10 +58,6 @@ const matching = {
                             connection.query(sql2, [plan], (err, res) => {
                                 if (err)
                                     return connection.rollback(() => { throw err });
-
-                                connection.query(sql3, [prefer], (err, res) => {
-                                    if(err)
-                                        return connection.rollback(() => { throw err });
                                     
                                     connection.commit((err) => {
                                         if (err)
@@ -72,7 +66,6 @@ const matching = {
                                         resolve(res);
                                 })
                             })
-                        })
                     }
                     else {
                         console.log("connection error" + err)
@@ -259,7 +252,7 @@ const matching = {
         return new Promise((resolve, reject) => {
             db.getConnection((err, connection) => {
                 if (!err) {
-                    let sql = `DELETE FROM queue, schedule, prefer
+                    let sql = `DELETE FROM queue, schedule
                                USING SCHEDULE_TB AS schedule
                                INNER JOIN REVIEWEE_PREFER_TB AS prefer
                                ON schedule.ID_PK = prefer.REVIEWEE_ID_FK
