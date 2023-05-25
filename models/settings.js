04af91a3696cb446df9d7f5cf8bdc2084cd2b0ee
김경환 <sksmsspdlvmf@naver.com>
2023-05-26 03:41:16 +0900

    회원가입

--- models/settings.js
+++ models/settings.js
@@ -2,16 +2,16 @@ const db = require("../config/database.js");
 const setProfileImage = require("../others/setProfileImage");
 
 const settings = {
-    getByLoginInfo: (userInfo, languageInfo, activityInfo) => {
-        return new Promise((resolve, reject) => {
-            let user = Object.values(userInfo);
-            let language = Object.values(languageInfo);
-            let activity = Object.values(activityInfo);
+  getByLoginInfo: (userInfo, languageInfo, activityInfo) => {
+    return new Promise((resolve, reject) => {
+      let user = Object.values(userInfo);
+      let language = Object.values(languageInfo);
+      let activity = Object.values(activityInfo);
 
-            db.getConnection((err, connection) => {
-                connection.beginTransaction((err) => {
-                    if (!err) {
-                        let sql1 = `SELECT
+      db.getConnection((err, connection) => {
+        connection.beginTransaction((err) => {
+          if (!err) {
+            let sql1 = `SELECT
                                         GITHUB,
                                         NICKNAME,
                                         PROFILE_IMG
@@ -19,7 +19,7 @@ const settings = {
                                         USER_TB 
                                     WHERE
                                         ID like '${id}'`;
-                        let sql2 = `SELECT
+            let sql2 = `SELECT
                                         C,
                                         CPLUS,
                                         CSHARP,
@@ -35,7 +35,7 @@ const settings = {
                                         LANGUAGE_TB
                                     WHERE
                                         id LIKE '${id}'`;
-                        let sql3 = `SELECT
+            let sql3 = `SELECT
                                         CODEREVIEW,
                                         REFACTORING,
                                         QA
@@ -44,88 +44,100 @@ const settings = {
                                     WHERE
                                         id LIKE '${id}'`;
 
-                        connection.query(sql1, (err, res) => {
-                            if (err)
-                                return connection.rollback(() => { throw err });
-                            
-                            connection.query(sql2, (err, res) => {
-                                if (err)
-                                    return connection.rollback(() => { throw err });
+            connection.query(sql1, (err, res) => {
+              if (err)
+                return connection.rollback(() => {
+                  throw err;
+                });
 
-                                connection.query(sql3, (err, res) => {
-                                    if (err)
-                                        return connection.rollback(() => { throw err });
+              connection.query(sql2, (err, res) => {
+                if (err)
+                  return connection.rollback(() => {
+                    throw err;
+                  });
 
-                                    connection.commit((err) => {
-                                        if (err)
-                                            return connection.rollback(() => { throw err });
-                                    })
-                                    resolve(res);
-                                })
-                            })
-                        })
-                    }
-                    else {
-                        console.log("connection error" + err)
+                connection.query(sql3, (err, res) => {
+                  if (err)
+                    return connection.rollback(() => {
+                      throw err;
+                    });
+
+                  connection.commit((err) => {
+                    if (err)
+                      return connection.rollback(() => {
                         throw err;
-                    }
-                })
+                      });
+                  });
+                  resolve(res);
+                });
+              });
+            });
+          } else {
+            console.log("connection error" + err);
+            throw err;
+          }
+        });
+      });
+    });
+  },
 
-            })
-        })
-    },
+  updateUserInfo: (userInfo, languageInfo, activityInfo) => {
+    return new Promise((resolve, reject) => {
+      let user = Object.values(userInfo);
+      let language = Object.values(languageInfo);
+      let activity = Object.values(activityInfo);
 
-    updateUserInfo: (userInfo, languageInfo, activityInfo) => {
-        return new Promise((resolve, reject) => {
-            let user = Object.values(userInfo);
-            let language = Object.values(languageInfo);
-            let activity = Object.values(activityInfo);
-
-            db.getConnection((err, connection) => {
-                connection.beginTransaction((err) => {
-                    if (!err) {
-                        let sql1 = `UPDATE USER_TB SET
+      db.getConnection((err, connection) => {
+        connection.beginTransaction((err) => {
+          if (!err) {
+            let sql1 = `UPDATE USER_TB SET
                                     PROFILE_IMG = ?, NICKNAME = ?, GITHUB = ?
                                     WHERE ID LIKE '${id}'`;
-                        let sql2 = `UPDATE LANGUAGE_TB SET
+            let sql2 = `UPDATE LANGUAGE_TB SET
                                     C = ?, CPLUS = ?, CSHARP = ?, JAVA = ?, 
                                     KOTLIN = ?, SWIFT = ?, PYTHON = ?, GO = ? 
                                     JAVASCRIPT = ?, RUST = ?, RUBY = ?
                                     WHERE id LIKE '${id}'`;
-                        let sql3 = `UPDATE LANGUAGE_TB SET
+            let sql3 = `UPDATE LANGUAGE_TB SET
                                     CODEREVIEW = ?, REFACTORING = ?, QA = ?
                                     WHERE id LIKE '${id}'`;
 
-                        connection.query(sql1, user, (err, res) => {
-                            if (err)
-                                return connection.rollback(() => { throw err });
-                            
-                            connection.query(sql2, language, (err, res) => {
-                                if (err)
-                                    return connection.rollback(() => { throw err });
+            connection.query(sql1, user, (err, res) => {
+              if (err)
+                return connection.rollback(() => {
+                  throw err;
+                });
 
-                                connection.query(sql3, activity, (err, res) => {
-                                    if (err)
-                                        return connection.rollback(() => { throw err });
+              connection.query(sql2, language, (err, res) => {
+                if (err)
+                  return connection.rollback(() => {
+                    throw err;
+                  });
 
-                                    connection.commit((err) => {
-                                        if (err)
-                                            return connection.rollback(() => { throw err });
-                                    })
-                                    resolve(res);
-                                })
-                            })
-                        })
-                    }
-                    else {
-                        console.log("connection error" + err)
+                connection.query(sql3, activity, (err, res) => {
+                  if (err)
+                    return connection.rollback(() => {
+                      throw err;
+                    });
+
+                  connection.commit((err) => {
+                    if (err)
+                      return connection.rollback(() => {
                         throw err;
-                    }
-                })
-
-            })
-        })
-    }
-}
+                      });
+                  });
+                  resolve(res);
+                });
+              });
+            });
+          } else {
+            console.log("connection error" + err);
+            throw err;
+          }
+        });
+      });
+    });
+  },
+};
 
 module.exports = settings;
--- routers/signup.js
+++ routers/signup.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const user = require('../models/user');
-const bcrypt = require('bcrypt');
-const setProfileImage = require('../others/setProfileImage');
+const user = require("../models/user");
+const bcrypt = require("bcrypt");
+const setProfileImage = require("../others/setProfileImage");
 
 //회원가입
 //이메일 인증은 어떻게 구현하지?
 //일단 나중에 고려하자
 
 // http://localhost:8080/signup
-router.get('/', async (req, res) => {
-    res.render('signup/signup.html');
-})
+router.get("/", async (req, res) => {
+  res.render("signup/signup.html");
+});
 
 //test admin 생성용
 // {
@@ -40,87 +40,92 @@ router.get('/', async (req, res) => {
 // }
 
 // http://localhost:8080/signup
-router.post('/', async (req, res) => {
-    conso
-    let userInfo = {
-        id : req.body.id,
-        encryptPwd : await bcrypt.hash(req.body.pwd, 12),
-        profileImg : setProfileImage(req.body.profileImgNumber),
-        nickname : req.body.nickname,
-        email : req.body.email,
-        github : req.body.github,
-        is_admin : 0,
-        authentication : 1
-    }
+router.post("/", async (req, res) => {
+  let userInfo = {
+    id: req.body.id,
+    encryptPwd: await bcrypt.hash(req.body.pwd, 12),
+    profileImg: setProfileImage(req.body.profileImgNumber),
+    nickname: req.body.nickname,
+    email: req.body.email,
+    github: req.body.github,
+    is_admin: 0,
+    authentication: 1,
+  };
 
-    let languageInfo = {
-        id : req.body.id,
-        c : req.body.c,
-        cPlus : req.body.cPlus,
-        cSharp : req.body.cSharp,
-        java : req.body.java,
-        kotlin : req.body.kotlin,
-        swift : req.body.swift,
-        python : req.body.python,
-        go : req.body.go,
-        javascript : req.body.javascript,
-        rust : req.body.rust,
-        ruby : req.body.ruby
-    }
+  let languageInfo = {
+    id: req.body.id,
+    c: req.body.c,
+    cPlus: req.body.cPlus,
+    cSharp: req.body.cSharp,
+    java: req.body.java,
+    kotlin: req.body.kotlin,
+    swift: req.body.swift,
+    python: req.body.python,
+    go: req.body.go,
+    javascript: req.body.javascript,
+    rust: req.body.rust,
+    ruby: req.body.ruby,
+  };
 
-    let activityInfo = {
-        id : req.body.id,
-        codereview : req.body.codereview,
-        refactoring : req.body.refactoring,
-        qa : req.body.qa
-    }
+  let activityInfo = {
+    id: req.body.id,
+    codereview: req.body.codereview,
+    refactoring: req.body.refactoring,
+    qa: req.body.qa,
+  };
 
-    //데이터 삽입이 안됐을 경우에는?
-    await user.signUp.setUserInfo(userInfo, languageInfo, activityInfo);
+  //데이터 삽입이 안됐을 경우에는?
+  await user.signUp.setUserInfo(userInfo, languageInfo, activityInfo);
 
-    res.redirect('/login');
-})
-
-// 미구현
-router.get('/email', (req, res) => {
-    //페이지 렌더링
-    res.render('email/email');
-})
+  res.redirect("/login");
+});
 
 // 미구현
-router.post('/email', async (req, res) => {
+router.get("/email", (req, res) => {
+  //페이지 렌더링
+  res.render("email/email");
+});
 
-    let generateRandom = function (min, max) {
-        let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-        return randomNumber;
-    }
+// 미구현
+router.post("/email", async (req, res) => {
+  let generateRandom = function (min, max) {
+    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    return randomNumber;
+  };
 
-    const auth = {
-        SendEmail: async (req, res) => {
-            const number = generateRandom(111111, 999999)
+  const auth = {
+    SendEmail: async (req, res) => {
+      const number = generateRandom(111111, 999999);
 
-            const sendEmail = req.body.sendEmail;
+      const sendEmail = req.body.sendEmail;
 
-            const mailOptions = {
-                //임시 이메일, 나중에 바꿔야함
-                from: "test@gmail.com",
-                to: sendEmail,
-                subject: "[onecastle]Authentication code",
-                text: "If you proceed one more step, your authentication will be completed. Please enter the following authentication code: " + number
-            };
+      const mailOptions = {
+        //임시 이메일, 나중에 바꿔야함
+        from: "test@gmail.com",
+        to: sendEmail,
+        subject: "[onecastle]Authentication code",
+        text:
+          "If you proceed one more step, your authentication will be completed. Please enter the following authentication code: " +
+          number,
+      };
 
-            await smtpTransport.sendMail(mailOptions, (err, res) => {
-                try {
-                    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMsg.AUTH_EMAIL_SUCCESS, {
-                        number: number
-                    }))
-                }
-                catch(err)  {
-                    res.status(statusCode.OK).send(util.fail(statusCode.BAD_REQUEST, responseMsg.AUTH_EMAIL_FAIL))
-                }
+      await smtpTransport.sendMail(mailOptions, (err, res) => {
+        try {
+          res.status(statusCode.OK).send(
+            util.success(statusCode.OK, responseMsg.AUTH_EMAIL_SUCCESS, {
+              number: number,
             })
+          );
+        } catch (err) {
+          res
+            .status(statusCode.OK)
+            .send(
+              util.fail(statusCode.BAD_REQUEST, responseMsg.AUTH_EMAIL_FAIL)
+            );
         }
-    }
-})
+      });
+    },
+  };
+});
 
 module.exports = router;
